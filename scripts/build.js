132e4fd881a60f43e1b9d2810dbd44406f6f68b0
jeroentvb <jeroentvb@hotmail.com>
2019-04-18 10:36:12 +0200

    Added js and css minification to the build task

--- scripts/build.js
+++ scripts/build.js
@@ -1,46 +1,108 @@
 const fs = require('fs-extra')
 const path = require('path')
 const ejs = require('ejs')
+const Terser = require('terser')
+const postcss = require('postcss')
+const cssnano = require('cssnano')
 
-fs.readdir('./src/pages/', (err, files) => {
-  if (err) throw new Error(err)
-
-  createPublicFolder()
-  copyAssets()
-  compileTemplates(files)
-})
-
-function createPublicFolder () {
-  if (!fs.existsSync('./public')) fs.mkdirSync('./public')
+function createDistFolder () {
+  if (!fs.existsSync('./dist')) {
+    fs.mkdirSync('./dist')
+    console.log('Created /dist folder')
+  }
+  if (!fs.existsSync('./dist/assets')) {
+    fs.mkdirSync('./dist/assets')
+    console.log('Created /dist/assets folder')
+  }
 }
 
-function copyAssets () {
-  fs.copy('./src/assets', './public/assets', err => {
-    if (err) console.error(`Assets could not be copied because of the following error: ${err}`)
+function copyAssets (type) {
+  fs.copy(`./src/assets/${type}`, `./dist/assets/${type}`, err => {
+    if (err) {
+      console.error(`${type} could not be copied because of the following error: ${err}`)
+    } else {
+      console.log(`Succesfully copied ${type}`)
+    }
   })
 }
 
-function compileTemplates (files) {
-  files.forEach(file => {
-    fs.readFile(path.resolve(`./src/pages/${file}`), 'utf8', (err, data) => {
-      if (err) console.error(err)
+function compileTemplates () {
+  fs.readdir('./src/pages/', (err, files) => {
+    if (err) throw new Error(err)
 
-      let template = ejs.compile(data, {
-        filename: `./src/pages/${file}`
-      })
+    files.forEach(file => {
+      fs.readFile(path.resolve(`./src/pages/${file}`), 'utf8', (err, data) => {
+        if (err) console.error(err)
 
-      const name = file === 'index.ejs' ? 'Home' : file.charAt(0).toUpperCase() + file.substr(1).replace('.ejs', '')
-      const html = template({
-        pagename: name
-      })
+        let template = ejs.compile(data, {
+          filename: `./src/pages/${file}`
+        })
 
-      fs.writeFile(`./public/${file.replace('.ejs', '')}.html`, html, err => {
-        if (err) {
-          console.error(err)
-        } else {
-          console.log('Compiled succesfully')
-        }
+        const name = file === 'index.ejs' ? 'Home' : file.charAt(0).toUpperCase() + file.substr(1).replace('.ejs', '')
+        const html = template({
+          pagename: name
+        })
+
+        fs.writeFile(`./dist/${file.replace('.ejs', '')}.html`, html, err => {
+          if (err) {
+            console.error(err)
+          } else {
+            console.log('Compiled succesfully')
+          }
+        })
       })
     })
   })
 }
+
+const minify = {
+  js: () => {
+    fs.readdir('./src/assets/js', (err, files) => {
+      files.forEach(file => {
+        if (err) throw new Error(err)
+        if (!fs.existsSync('./dist/assets/js')) fs.mkdirSync('./dist/assets/js')
+
+        fs.readFile(`./src/assets/js/${file}`, 'utf8', (err, data) => {
+          if (err) throw new Error(err)
+
+          const code = Terser.minify(data)
+
+          fs.writeFile(`./dist/assets/js/${file}`, code.code, err => {
+            if (err) {
+              console.error(err)
+            } else {
+              console.log('Minified JS')
+            }
+          })
+        })
+      })
+    })
+  },
+  css: () => {
+    fs.readdir('./src/assets/css', (err, files) => {
+      if (err) throw new Error(err)
+      if (!fs.existsSync('./dist/assets/css')) fs.mkdirSync('./dist/assets/css')
+
+      files = files.filter(file => file.slice(-4) === '.css')
+
+      files.forEach(file => {
+        fs.readFile(`./src/assets/css/${file}`, 'utf8', (err, css) => {
+          if (err) throw new Error(err)
+
+          postcss([cssnano])
+            .process(css, { from: undefined, to: undefined })
+            .then(result => fs.writeFile(`./dist/assets/css/${file}`, result.css, () => true))
+            .then(() => console.log('Minified CSS'))
+            .catch(err => console.error(`The css could not be minified because of the following error: ${err}`))
+        })
+      })
+    })
+  }
+}
+
+createDistFolder()
+copyAssets('fonts')
+copyAssets('img')
+minify.js()
+minify.css()
+compileTemplates()
